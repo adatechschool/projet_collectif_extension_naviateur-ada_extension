227a07ff70df1d8fdc8577fe0a2640e7e52ef667
DAN-ASUS\alexl <alexluu13@gmail.com>
2022-09-08 17:05:45 +0200

    local storage alex

--- extensionTurfu/popup.js
+++ extensionTurfu/popup.js
@@ -1,1 +1,6 @@
+// if (localStorage.getItem("nom") != null)
+//     h1.textContent = `Bonjour ${localStorage.getItem("nom")}`;
 
+//     bouton.onclick = () =>{
+//         localStorage.setItem("nom",nom.value);
+//     }
