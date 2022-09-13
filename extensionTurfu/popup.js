ff103047ba9e4ef2fededea0710f1cdfbefbed48
DAN-ASUS\alexl <alexluu13@gmail.com>
2022-09-13 10:41:17 +0200

    modif main search bar + clean

--- extensionTurfu/popup.js
+++ extensionTurfu/popup.js
@@ -1,9 +1,3 @@
-// if (localStorage.getItem("nom") != null)
-//     h1.textContent = `Bonjour ${localStorage.getItem("nom")}`;
-
-//     bouton.onclick = () =>{
-//         localStorage.setItem("nom",nom.value);
-//     }
 // SEARCH BAR
 const icon = document.querySelector(".icon");
 const search = document.querySelector(".search");
@@ -11,15 +5,4 @@ icon.onclick = () => {
   search.classList.toggle("active");
 };
 
-// LOCAL STORAGE
-// Stocker indéfiniment - 10mb max -- CHROME only
-//localStorage.setItem("alex", "delhia");
-//localStorage.removeItem("alex");
 
-mysearch.onclick = () => {
-  console.log("coucou");
-  let textUser = document.getElementById("mysearch").value;
-  if (textUser != null) console.log(textUser);
-  console.log(document.getElementById("mysearch").value);
-  localStorage.setItem("name", document.getElementById("mysearch").value);
-};
