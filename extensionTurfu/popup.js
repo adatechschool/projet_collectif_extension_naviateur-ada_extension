a161b7a442b3932a337e1a34fbf47d18f8a3f20e
DAN-ASUS\alexl <alexluu13@gmail.com>
2022-09-14 21:57:47 +0200

    petit modif + highlight

--- extensionTurfu/popup.js
+++ extensionTurfu/popup.js
@@ -1,11 +1,7 @@
-// SEARCH BAR
+///////////////////////// SEARCH BAR /////////////////////////
 const icon = document.querySelector(".icon");
 const search = document.querySelector(".search");
 icon.onclick = () => {
-  search.classList.toggle("active");
+search.classList.toggle("active");
 };
 
-const btn = document.getElementById("clearbtn");
-btn.onclick = () => {
-  document.getElementById("mySearch").value = "";
-};
