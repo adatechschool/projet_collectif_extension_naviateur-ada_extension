42efd2480ba902ef610143525b2b6d3af3aac1f4
DAN-ASUS\alexl <alexluu13@gmail.com>
2022-09-15 10:29:25 +0200

    api

--- extensionTurfu/popup.js
+++ extensionTurfu/popup.js
@@ -9,3 +9,34 @@ const btn = document.getElementById("clearbtn");
 btn.onclick = () => {
   document.getElementById("mySearch").value = "";
 };
+
+//ENTER
+document.onkeydown = function () {
+  if (window.event.keyCode == "13") {
+    dictionary()
+  }
+}
+
+//DICTIONARY
+async function dictionary (){
+  let define = document.querySelector("#mySearch").value
+  console.log(define)
+  let url=`https://api.dictionaryapi.dev/api/v2/entries/en/${define}`
+  const response = await fetch(url,{
+    method:"GET"
+  })
+  const data = await response.json()
+  console.log(data[0].meanings[0].definitions[0].definition)
+  let answer = prompt( "is it a noun(n) a verb(v) or an adjective(a)?")
+  if(answer=="n"){
+    alert(data[0].meanings[0].definitions[0].definition)
+  }else if(answer=="v")
+  {
+    alert(data[0].meanings[1].definitions[0].definition)
+  }else if(answer=="a")
+  {
+    alert(data[0].meanings[2].definitions[0].definition)
+  }else{
+    prompt( "is it a noun(n) a verb(v) or an adjective(a)?")
+  }
+}
